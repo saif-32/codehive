e625f5a124a94524080969f89035f76e1a233c77
saif.pirani <saif.pirani@caci.com>
2023-06-07 15:25:50 -0500

    Connected Front-End Login to Back-End

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,75 +1,130 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { NavLink } from 'react-router-dom';
+import { useCookies } from "react-cookie";
 
-
+// Hello</div>) : <div>Hello</div>}
 
 export const Navbar = () => {
-    return (
-    <nav>
-          {
-            <header class="p-3 text-bg-dark">
-                <div class="container">
-                <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                    <img src="logo.png" width="60" height="60"></img>
-                    <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"></a>
-
-                    <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                    <li id="hello"><NavLink 
-                        to="/" 
-                        className={({ isActive, isPending }) =>
-                            isPending ? "nav-link px-2 text-white" : isActive ? "nav-link px-2" : "nav-link px-2 text-white"
-                        }>
-                            Home
-                    </NavLink></li>
-                    <li><NavLink 
-                        to="/register" 
-                        className={({ isActive, isPending }) =>
-                            isPending ? "nav-link px-2 text-white" : isActive ? "nav-link px-2" : "nav-link px-2 text-white"
-                        }>
-                            Register
-                    </NavLink></li>
-                    <li><NavLink 
-                        to="/login" 
-                        className={({ isActive, isPending }) =>
-                            isPending ? "nav-link px-2 text-white" : isActive ? "nav-link px-2" : "nav-link px-2 text-white"
-                        }>
-                            Login
-                    </NavLink></li>
-                    <li><NavLink 
-                        to="/profile" 
-                        className={({ isActive, isPending }) =>
-                            isPending ? "nav-link px-2 text-white" : isActive ? "nav-link px-2" : "nav-link px-2 text-white"
-                        }>
-                            Profile
-                    </NavLink></li>
-                    <li><NavLink 
-                        to="/discover" 
-                        className={({ isActive, isPending }) =>
-                            isPending ? "nav-link px-2 text-white" : isActive ? "nav-link px-2" : "nav-link px-2 text-white"
-                        }>
-                            Discover
-                    </NavLink></li>
-                    </ul>
-
-                    <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-                    <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search"></input>
-                    </form>
-
-
-                    <div class="text-end">
-                        <Link to="/login" >
-                            <button type="button" onclick=<Link to="/login" ></Link> class="btn btn-outline-light me-2">Login</button>
-                        </Link>
-                        <Link to="/register" >
-                            <button type="button" class="btn btn-warning">Sign Up</button>
-                        </Link>
-                    </div>
-                </div>
+    const [cookies, setCookies] = useCookies(["access_token"]);
+    const navigate = useNavigate();
+    const logout = () => {
+        setCookies("access_token", "");
+        window.localStorage.removeItem("userID");
+        navigate("/")
+    };
+    return ( 
+    
+    <div>
+        {!cookies.access_token ? (
+            // If user is not signed in, display this navbar.
+            <div> 
+                <nav>
+                    {
+                        <header class="p-3 text-bg-dark">
+                            <div class="container">
+                            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                                <img src="logo.png" width="60" height="60"></img>
+                                <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"></a>
+            
+                                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                                <li id="hello"><NavLink 
+                                    to="/" 
+                                    className={({ isActive, isPending }) =>
+                                        isPending ? "nav-link px-2 text-white" : isActive ? "nav-link px-2" : "nav-link px-2 text-white"
+                                    }>
+                                        Home
+                                </NavLink></li>
+                                <li><NavLink 
+                                    to="/register" 
+                                    className={({ isActive, isPending }) =>
+                                        isPending ? "nav-link px-2 text-white" : isActive ? "nav-link px-2" : "nav-link px-2 text-white"
+                                    }>
+                                        Register
+                                </NavLink></li>
+                                <li><NavLink 
+                                    to="/login" 
+                                    className={({ isActive, isPending }) =>
+                                        isPending ? "nav-link px-2 text-white" : isActive ? "nav-link px-2" : "nav-link px-2 text-white"
+                                    }>
+                                        Login
+                                </NavLink></li>
+                                </ul>
+            
+                                <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+                                <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search"></input>
+                                </form>
+            
+            
+                                <div class="text-end">
+                                    <Link to="/login" >
+                                        <button type="button" onclick=<Link to="/login" ></Link> class="btn btn-outline-light me-2">Login</button>
+                                    </Link>
+                                    <Link to="/register" >
+                                        <button type="button" class="btn btn-warning">Sign Up</button>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                        </header>
+                    }
+                </nav>
             </div>
-            </header>
+    ) :
+            // If user is signed in, display this navbar.
+            <div> 
+                <nav>
+                    {
+                        <header class="p-3 text-bg-dark">
+                            <div class="container">
+                            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                                <img src="logo.png" width="60" height="60"></img>
+                                <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"></a>
+            
+                                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                                <li id="hello"><NavLink 
+                                    to="/" 
+                                    className={({ isActive, isPending }) =>
+                                        isPending ? "nav-link px-2 text-white" : isActive ? "nav-link px-2" : "nav-link px-2 text-white"
+                                    }>
+                                        Home
+                                </NavLink></li>
+                                <li><NavLink 
+                                    to="/profile" 
+                                    className={({ isActive, isPending }) =>
+                                        isPending ? "nav-link px-2 text-white" : isActive ? "nav-link px-2" : "nav-link px-2 text-white"
+                                    }>
+                                        Profile
+                                </NavLink></li>
+                                <li><NavLink 
+                                    to="/discover" 
+                                    className={({ isActive, isPending }) =>
+                                        isPending ? "nav-link px-2 text-white" : isActive ? "nav-link px-2" : "nav-link px-2 text-white"
+                                    }>
+                                        Discover
+                                </NavLink></li>
+                                </ul>
+            
+                                <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+                                <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search"></input>
+                                </form>
+            
+            
+                                <div class="text-end">
+                                    <Link to="/login" >
+                                        <button type="button" onclick=<Link to="/login" ></Link> class="btn btn-outline-light me-2">Profile</button>
+                                    </Link>
+                                    <button type="button" onClick={logout} class="btn btn-warning">Log Out</button>
+                                </div>
+                            </div>
+                        </div>
+                        </header>
+                    }
+                </nav>
+            </div>
         }
-    </nav>
-    );
-}
+    </div>
+        );
+    }
+
 
 
--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,14 +1,52 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom"
+import { useCookies } from "react-cookie"
+import axios from 'axios'
+
 export const Login = () => {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [_, setCookies] = useCookies(["access_token"])
+    const navigate = useNavigate();
+    const onSubmit = async (event) => {
+      event.preventDefault();
+      try {
+        const response = await axios.post("http://localhost:3001/auth/login", {
+          username,
+          password
+        });
+
+        setCookies("access_token", response.data.token);
+        window.localStorage.setItem("userID", response.data.userID);
+        navigate("/")
+
+        alert("Login Attempt was sent successfully.")
+      } catch (err) {
+        console.error(err)
+      }
+    
+    };
+
+
     return <div>
     <div className="login-container">
       <div className="login-card">
         <h1>Login to Code<span className="yellow">Hive</span></h1>
-        <form className="login-form">
-          <input type="text" placeholder="Username" />
-          <input type="password" placeholder="Password" />
+
+        <form className="login-form" onSubmit={onSubmit}>
+
+          <label htmlFor="username"></label>
+          <input type="text" id="username" placeholder="Username" value={username} onChange={(event) => setUsername(event.target.value)}/>
+          <label htmlFor="password"></label>
+          <input type="password" id="password" placeholder="Password" value={password} onChange={(event) => setPassword(event.target.value)}/>
+
+
           <a href="#" className="forgot-link">Forgot Username or Password?</a>
           <button type="submit">Log In</button>
+
         </form>
+
+
         <p className="signup-link">Don't have an account? <a href="#">Sign Up</a></p>
         
         <div class="or-seperator"><h3>Or Continue With</h3></div>
@@ -30,23 +68,9 @@ export const Login = () => {
                 <span class="fa fa-linkedin"></span> Login with LinkedIn
           </a>
         </div>
-{/* 
-        <a href="#" class="social-button">
-          <img src="logo.png" class="social-logo"></img>
-          <span class="button-text">Login with Github</span>
-        </a>
-
-
-        <a href="#" class="social-button">
-          <i class="fa fa-google"></i>
-          <img src="logo.png" class="social-logo"></img>
-          <span class="button-text">Login with LinkedIn</span>
-        </a> */}
-
 
       </div>
     </div>
   </div>
 };
-
 

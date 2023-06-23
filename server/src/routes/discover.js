cb8a73b42395f6c39a2d5153dc371d6a2480e585
saif-32 <saifpirani@gmail.com>
2023-06-22 21:55:37 -0500

    Discover Page Update

--- server/src/routes/discover.js
+++ server/src/routes/discover.js
@@ -24,4 +24,38 @@ router.post("/users/school", async (req, res) => {
     }
 });
 
+router.post("/users/level", async (req, res) => {
+    try {
+        const { userLevel } = req.body;
+        const users = await UserModel.find({university: userUniversity}, 'firstName lastName username age university skillLevel languages interests');
+        const count = users.length;
+        res.json({ count, users });
+    } catch (err) {
+        res.json(err);
+    }
+});
+
+router.post("/users/interests", async (req, res) => {
+    try {
+        const { userInterests } = req.body;
+        const users = await UserModel.find({university: userUniversity}, 'firstName lastName username age university skillLevel languages interests');
+        const count = users.length;
+        res.json({ count, users });
+    } catch (err) {
+        res.json(err);
+    }
+});
+
+router.post("/users/languages", async (req, res) => {
+    try {
+        const { userLanguages } = req.body;
+        const users = await UserModel.find({university: userUniversity}, 'firstName lastName username age university skillLevel languages interests');
+        const count = users.length;
+        res.json({ count, users });
+    } catch (err) {
+        res.json(err);
+    }
+});
+
+
 export { router as discoverRouter };

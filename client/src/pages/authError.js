3f0632de754063667888f3a2b6e9f0f8b5858e8d
saif-32 <saifpirani@gmail.com>
2023-06-26 23:33:30 -0500

    Registration Auth Error Page Fixed

--- client/src/pages/authError.js
+++ client/src/pages/authError.js
@@ -1,14 +1,17 @@
 import '../styles/email.css'
+import { Link} from "react-router-dom"
 
 export const AuthError = () => {
 
-    return <div>
-    <div className="email-container">
-      <img src='https://cdn.discordapp.com/attachments/798251319847813200/1114605006927184073/CodeHive-Logo-Isolated-3.png'></img>
-      <div className="email-card">
-        <h1 className='light-yellow'>We apologize but something went wrong!</h1>
-        <h3>Please try registering within our website if the problem persists.</h3>
-      </div>
-    </div>
-  </div>
+    return <div className='discover-background'> 
+    <div className='discover-container'>
+                        <div className="discover-invalid-container">
+                            <img src='https://cdn.discordapp.com/attachments/798251319847813200/1114605006927184073/CodeHive-Logo-Isolated-3.png' alt="logo"></img>
+                            <div className="discover-invalid">
+                            <h1>Registration Error</h1>
+                            <h2>We apologize but something went wrong. Please try registering within our website if the problem persists or contact us at help@codehive.com</h2>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
 };
